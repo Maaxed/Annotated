392ed29fc8637007741a7c85b53b3deb838cd0c5
LeBossMax2 <spb.max2@gmail.com>
2018-12-24 20:32:08 +0100

    Adding @Generated annotation

--- processor/src/main/java/fr/max2/packeta/processor/network/PacketProcessor.java
+++ processor/src/main/java/fr/max2/packeta/processor/network/PacketProcessor.java
@@ -54,7 +54,6 @@ import javax.tools.JavaFileObject;
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class PacketProcessor extends AbstractProcessor
 {
-	//TODO [v1.0] add @Generated annotation
 	private DataHandlerParameters.Finder finder;
 	
 	@Override
