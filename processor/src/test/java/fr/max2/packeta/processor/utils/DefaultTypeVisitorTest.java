f707f012645ec1ebf2ceae49f430947df381cf56
LeBossMax2 <spb.max2@gmail.com>
2019-02-04 00:03:35 +0100

    Add DefaultTypeVisitor and ExceptionUtils unit tests

---
+++ processor/src/test/java/fr/max2/packeta/processor/utils/DefaultTypeVisitorTest.java
@@ -0,0 +1,124 @@
+package fr.max2.packeta.processor.utils;
+
+import static org.junit.Assert.*;
+
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVisitor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class DefaultTypeVisitorTest
+{
+	private TestingType passedType;
+	
+	@Before
+	public void setUp()
+	{
+		passedType = new TestingType();
+	}
+	
+	@Test
+	public void testVisit()
+	{
+		TestingVisitor visitor = new TestingVisitor();
+		passedType = new TestingType()
+		{
+			@Override
+			public <R, P> R accept(TypeVisitor<R, P> v, P p)
+			{
+				assertSame(visitor, v);
+				return v.visitUnknown(this, p);
+			}
+		};
+		
+		assertEquals("Output", visitor.visit(passedType, "Input"));
+		assertEquals("Error", visitor.visit(passedType));
+	}
+	
+	@Test
+	public void testVisitPrimitive()
+	{
+		assertEquals("Output", new TestingVisitor().visitPrimitive(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitNull()
+	{
+		assertEquals("Output", new TestingVisitor().visitNull(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitArray()
+	{
+		assertEquals("Output", new TestingVisitor().visitArray(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitDeclared()
+	{
+		assertEquals("Output", new TestingVisitor().visitDeclared(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitError()
+	{
+		assertEquals("Output", new TestingVisitor().visitError(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitTypeVariable()
+	{
+		assertEquals("Output", new TestingVisitor().visitTypeVariable(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitWildcard()
+	{
+		assertEquals("Output", new TestingVisitor().visitWildcard(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitExecutable()
+	{
+		assertEquals("Output", new TestingVisitor().visitExecutable(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitNoType()
+	{
+		assertEquals("Output", new TestingVisitor().visitNoType(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitUnion()
+	{
+		assertEquals("Output", new TestingVisitor().visitUnion(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitIntersection()
+	{
+		assertEquals("Output", new TestingVisitor().visitIntersection(passedType, "Input"));
+	}
+	
+	@Test
+	public void testVisitUnknown()
+	{
+		assertEquals("Output", new TestingVisitor().visitUnknown(passedType, "Input"));
+	}
+	
+	private class TestingVisitor implements DefaultTypeVisitor<String, String>
+	{
+
+		@Override
+		public String visitDefault(TypeMirror e, String p)
+		{
+			assertSame(passedType, e);
+			return p == "Input" ? "Output" : "Error";
+		}
+		
+	}
+	
+}
---
+++ processor/src/test/java/fr/max2/packeta/processor/utils/TestingType.java
@@ -0,0 +1,169 @@
+package fr.max2.packeta.processor.utils;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.ErrorType;
+import javax.lang.model.type.ExecutableType;
+import javax.lang.model.type.IntersectionType;
+import javax.lang.model.type.NoType;
+import javax.lang.model.type.NullType;
+import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVariable;
+import javax.lang.model.type.TypeVisitor;
+import javax.lang.model.type.UnionType;
+import javax.lang.model.type.WildcardType;
+
+import org.junit.Assert;
+
+
+public class TestingType implements PrimitiveType, NullType, ArrayType, ErrorType, TypeVariable, WildcardType, ExecutableType, NoType, UnionType, IntersectionType
+{
+
+	@Override
+	public List<? extends AnnotationMirror> getAnnotationMirrors()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public <A extends Annotation> A getAnnotation(Class<A> annotationType)
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public <A extends Annotation> A[] getAnnotationsByType(Class<A> annotationType)
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeKind getKind()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public <R, P> R accept(TypeVisitor<R, P> v, P p)
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public Element asElement()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getEnclosingType()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public List<? extends TypeMirror> getTypeArguments()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public List<? extends TypeMirror> getBounds()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public List<? extends TypeMirror> getAlternatives()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public List<? extends TypeVariable> getTypeVariables()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getReturnType()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public List<? extends TypeMirror> getParameterTypes()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getReceiverType()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public List<? extends TypeMirror> getThrownTypes()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getExtendsBound()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getSuperBound()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getUpperBound()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getLowerBound()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+
+	@Override
+	public TypeMirror getComponentType()
+	{
+		Assert.fail("Illegal method call");
+		return null;
+	}
+	
+	
+}
